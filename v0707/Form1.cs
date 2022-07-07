033faa00ad1f28d141f5921fb492dbde24ce904d
A601-03 <oshima.dat22@gmail.com>
2022-07-07 13:49:36 +0900

    Initial commit

---
+++ v0707/Form1.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace v0707
+{
+    public partial class Form1 : Form
+    {
+        int[] data = new int[3];
+        static Random rand = new Random();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            data[0] = rand.Next(11);
+            data[1] = rand.Next(11);
+            data[2] = rand.Next(11);
+            MessageBox.Show($"{data[0]},{data[1]},{data[2]}");
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            int gk;
+            gk = data[0] + data[1] + data[2];
+          
+            MessageBox.Show($"{gk}");
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            int ave;
+            ave =(data[0] + data[1] + data[2]) / 3;
+            MessageBox.Show($"{ ave}");
+        }
+    }
+}
